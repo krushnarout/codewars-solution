19e1faf167e7d915e4fb20e4436710ce1aa94269
Krushna Kanta Rout <krushnakantarout90@gmail.com>
2023-06-12 23:32:55 +0530

    Transportation-on-vacation.js

---
+++ 8kyu/Transportation-on-vacation.js
@@ -0,0 +1,20 @@
+// After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+// You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+// Write a code that gives out the total amount for different days(d).
+
+// My solution
+function rentalCarCost(d) {
+    var totalAmount = d * 40; // Base cost: $40 per day
+  
+    if (d >= 7) {
+      totalAmount -= 50;
+    } else if (d >= 3) {
+      totalAmount -= 20; 
+    }
+  
+    return totalAmount;
+  }
