0cfbb4f50e2ac57f8290284381405ce8cfa6f2db
Krushna Kanta Rout <krushnakantarout90@gmail.com>
2023-08-03 22:01:40 +0530

    Add All-Star-Challenge-18.js

---
+++ 8kyu/All-Star-Code-Challenge-18.js
@@ -0,0 +1,24 @@
+// Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+// If no occurrences can be found, a count of 0 should be returned.
+
+// ("Hello", "o")  ==>  1
+// ("Hello", "l")  ==>  2
+// ("", "z")       ==>  0
+// str_count("Hello", 'o'); // returns 1
+// str_count("Hello", 'l'); // returns 2
+// str_count("", 'z'); // returns 0
+// Notes
+// The first argument can be an empty string
+// In languages with no distinct character data type, the second argument will be a string of length 1
+
+// My solution
+function strCount(str, letter) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === letter) {
+        count++;
+      }
+    }
+    return count;
+  }
