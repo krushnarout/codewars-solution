60f741c1c4b2400241d715b7d7c29e6de1592e40
Krushna Kanta Rout <krushnakantarout90@gmail.com>
2023-08-17 21:58:34 +0530

    Add Welcome-to-the-City.js

---
+++ 8kyu/Welcome-to-the-City.js
@@ -0,0 +1,12 @@
+// Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+// Example:
+
+// ['John', 'Smith'], 'Phoenix', 'Arizona'
+// This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+
+// My solution
+function sayHello(name, city, state) {
+    const fullName = name.join(" ");
+    return `Hello, ${fullName}! Welcome to ${city}, ${state}!`;
+  }
