6843643caff930a1879e1a1dd837b1a847bedf0b
Krushna Kanta Rout <129386740+krushnarout@users.noreply.github.com>
2023-09-15 21:29:37 +0530

    Add Odd-or-Even.js

---
+++ 7kyu/Odd-or-Even.js
@@ -0,0 +1,29 @@
+// Task:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+// Have fun!
+
+// My solution
+function oddOrEven(array) {
+  
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum % 2 === 0 ? 'even' : 'odd';
+}
